f274a6d7cb661a3f594a79a970a5e01a3b1c1790
Humberto Trellez <htrellez@icloud.com>
2022-11-08 19:55:53 -0600

    change the manner that I print the song

--- src/main.rs
+++ src/main.rs
@@ -15,66 +15,38 @@ fn main() {
         "Twelfth"
     ];
 
+    let phrases: [&str; 12]  = [
+        "My good friends brought to me",
+        "Two candy canes",
+        "Three boughs of holly",
+        "Four colored lights",
+        "A shining star",
+        "Little silver bells",
+        "Candles a-glowing",
+        "Gold and silver tinsel",
+        "A guardian angel",
+        "Some mistletoe",
+        "Gifts for one and all",
+        "All their good wishes"
+    ];
+
     println!("The Twelve Days of Christmas");
     println!("");
 
+    let mut i: i32 = 0;
+
     for ordinal in ordinal_numbers.into_iter() {
-        println!("On the {} day of Christmas", ordinal);
         
-        match ordinal {
-            "First" | "Second" | "Third" | "Fourth" | "Fifth" | "Sixth" | "Seventh" | "Eighth" | "Ninth" | "Tenth" | "Eleventh" | "Twelfth" => {
-                println!("My good friends brought to me");
+        println!("On the {} day of Christmas", ordinal);
 
-                if ordinal.eq("Twelfth") {
-                    println!("All their good wishes");
-                }
+        i = i + 1;
 
-                if ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Gifts for one and all");
-                }
-
-                if ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Some mistletoe");
-                }
-
-                if ordinal.eq("Ninth") || ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("A guardian angel");
-                }
-
-                if ordinal.eq("Eighth") || ordinal.eq("Ninth") || ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Gold and silver tinsel");
-                }
-
-                if ordinal.eq("Seventh") || ordinal.eq("Eighth") || ordinal.eq("Ninth") ||ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Candles a-glowing");
-                }
-
-                if ordinal.eq("Sixth") || ordinal.eq("Seventh") || ordinal.eq("Eighth") || ordinal.eq("Ninth") || ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Little silver bells");
-                }
-
-                if ordinal.eq("Fifth") || ordinal.eq("Sixth") || ordinal.eq("Seventh") || ordinal.eq("Eighth") || ordinal.eq("Ninth") || ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("A shining star");
-                }
-
-                if ordinal.eq("Fourth") || ordinal.eq("Fifth") || ordinal.eq("Sixth") || ordinal.eq("Seventh") || ordinal.eq("Eighth") || ordinal.eq("Ninth") || ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Four colored lights");
-                }
-
-                if ordinal.eq("Third") || ordinal.eq("Fourth") || ordinal.eq("Fifth") || ordinal.eq("Sixth") || ordinal.eq("Seventh") || ordinal.eq("Eighth") || ordinal.eq("Ninth") ||ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Three boughs of holly");
-                }
-
-                if ordinal.eq("Second") || ordinal.eq("Third") || ordinal.eq("Fourth") || ordinal.eq("Fifth") || ordinal.eq("Sixth") || ordinal.eq("Seventh") || ordinal.eq("Eighth") || ordinal.eq("Ninth") ||ordinal.eq("Tenth") || ordinal.eq("Eleventh") || ordinal.eq("Twelfth") {
-                    println!("Two candy canes");
-                }
-
-                println!("And a song for the Christmas tree");
-                println!(""); 
-            },
-            _ => { 
-                println!(""); 
-            },
+        for _n in 0..i {
+            println!("{}", phrases[_n as usize]);
         }
+
+
+        println!("And a song for the Christmas tree");
+        println!(""); 
     }
 }
